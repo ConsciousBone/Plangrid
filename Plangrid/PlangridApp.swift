1da26ceb5e68a0eac14b0e5b9645f5b67ab5e1f2
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-04 17:40:16 +0000

    Initial Commit

---
+++ Plangrid/PlangridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlangridApp.swift
+//  Plangrid
+//
+//  Created by Evan Plant on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlangridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
