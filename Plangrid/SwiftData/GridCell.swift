f56cb6031acdc476660cb4b166026d9820f8de18
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-07 20:47:48 +0000

    Add placeholders to gridcell

--- Plangrid/SwiftData/GridCell.swift
+++ Plangrid/SwiftData/GridCell.swift
@@ -12,17 +12,17 @@ import SwiftData
     var column: Int
     var row: Int
     var iconIndex: Int // sf symbol, will pull from a list
-    var colourIndex: Int // swiftui, again will pull from list
+    var colourIndex: Int// swiftui, again will pull from list
     var name: String
     var notes: String
     
     init(
         column: Int,
         row: Int,
-        iconIndex: Int,
-        colourIndex: Int,
-        name: String,
-        notes: String
+        iconIndex: Int = 0,
+        colourIndex: Int = 0,
+        name: String = "",
+        notes: String = ""
     ){
         self.column = column
         self.row = row
