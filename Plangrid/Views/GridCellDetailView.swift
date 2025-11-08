86abc6e087d85de67fe36476ab99cb524085d677
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-08 13:42:11 +0000

    Refine detailview

--- Plangrid/Views/GridCellDetailView.swift
+++ Plangrid/Views/GridCellDetailView.swift
@@ -13,18 +13,43 @@ struct GridCellDetailView: View {
     var cellIconIndex: Int
     var cellColourIndex: Int
     
+    let cellIcons = [
+        "document", "clipboard", "book",
+        "clock", "soccerball", "rugbyball",
+        "tennisball", "flag", "bell",
+        "exclamationmark", "car", "bus",
+        "bicycle", "house", "building"
+    ]
+    let accentColours = [
+        Color.red.gradient, Color.orange.gradient,
+        Color.yellow.gradient, Color.green.gradient,
+        Color.mint.gradient, Color.blue.gradient,
+        Color.purple.gradient, Color.brown.gradient,
+        Color.white.gradient, Color.black.gradient
+    ]
+    
     var body: some View {
         Form {
             Section {
-                Text(cellName)
-            } header: {
-                Text("Name")
-            }
-            
-            Section {
-                Text(cellNotes)
-            } header: {
-                Text("Notes")
+                HStack {
+                    RoundedRectangle(cornerRadius: 17)
+                        .foregroundStyle(accentColours[cellColourIndex])
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: 100)
+                        .overlay {
+                            Image(systemName: "\(cellIcons[cellIconIndex])")
+                                .resizable()
+                                .foregroundStyle(.blue.adaptedTextColor())
+                                .scaledToFit()
+                                .padding(32)
+                        }
+                    VStack {
+                        Text(cellName)
+                        Text(cellNotes)
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding(5)
+                }
             }
         }
         .navigationTitle(cellName)
