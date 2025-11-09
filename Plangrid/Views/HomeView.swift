c61522a4ba232b906b21a22b9ffadc9ec2f7f60c
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-11-09 23:22:00 +0000

    Improve homeview (see desc)
    Add vars, add list, add greeting header

--- Plangrid/Views/HomeView.swift
+++ Plangrid/Views/HomeView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
+    // app version stuff, tyvm searchino!
+    let appDisplayName = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String ?? "Plangrid"
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "unknown"
+    let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "unknown"
+    
+    var greeting: String {
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        switch hour {
+        case 5..<12:
+            return "Good morning!"
+        case 12..<17:
+            return "Good afternoon!"
+        default:
+            return "Good evening!"
+        }
+    }
+    
     var body: some View {
-        Text("Home view")
+        Form {
+            Section {
+                Text(greeting)
+                    .font(.title)
+            } header: {
+                Text("\(appDisplayName) - version \(appVersion) build \(buildNumber)")
+            }
+        }
     }
 }
 
